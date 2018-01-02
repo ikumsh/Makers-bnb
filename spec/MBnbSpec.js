b5b3c1d60e00d7d1e9e954d7d952d9eac195a5b3
Valentina <valentina.rmo@gmail.com>
2018-01-02 14:36:56 +0000

    modifies test to test for our array of rental properties containing the same object we added

--- spec/MBnbSpec.js
+++ spec/MBnbSpec.js
@@ -1,8 +1,10 @@
 describe("MBnb", function() {
   var bnb;
+  var property;
 
   beforeEach(function(){
     bnb = new MBnb();
+    property = new Space(123,"title","desc","address","price");
 });
 
 
@@ -11,13 +13,14 @@ describe("MBnb", function() {
   });
 
   it("should be able to be added to", function() {
-    bnb.addToList("hello");
-    expect(bnb.list.length).toEqual(1);
+    bnb.addToList(property);
+    expect(bnb.list).toContain(jasmine.objectContaining(property));
   });
 
   it("should get the list of spaces", function(){
     bnb.addToList("hello");
     expect(bnb.getList()).toBe(bnb.list);
   });
+
 
 });
