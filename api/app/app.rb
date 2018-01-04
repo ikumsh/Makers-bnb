8a46a64c674d5e5297109d313af13353060c0969
George <george_whiting@hotmail.com>
2018-01-04 11:27:25 +0000

    finishes sign up functionality

--- api/app/app.rb
+++ api/app/app.rb
@@ -3,9 +3,15 @@ ENV['RACK_ENV'] ||= 'development'
 require 'sinatra/base'
 require 'json'
 require_relative 'setup'
+require 'sinatra/flash'
+require_relative 'helpers'
 
 class Mbnb < Sinatra::Base
   include BCrypt
+  register Sinatra::Flash
+  enable :sessions
+  set :session_secret, 'session'
+  helpers Helpers
 
   get '/' do
     redirect '/places'
@@ -23,6 +29,25 @@ class Mbnb < Sinatra::Base
   post '/newlisting' do
     place = Place.create(title: params[:title], description: params[:description], address: params[:address], price: params[:price])
     redirect '/places'
+  end
+
+  get '/users/new' do
+    erb :'/users/new'
+  end
+
+  post '/users' do
+    @user = User.create(email: params[:email],
+                name: params[:name],
+                password: params[:password],
+                password_confirmation: params[:password_confirmation])
+
+    if @user.save
+      session[:user_id] = @user.id
+      redirect '/places'
+    else
+      flash.now[:error] = @user.errors.values.join("<br>")
+      erb :'/users/new'
+    end
   end
 
   run if app_file == $PROGRAM_NAME
